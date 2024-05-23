baa58b283fb21f781341021815bb035912d6fdbb
Nirmal <akoliyanirmal43@gmail.com>
2024-05-23 10:35:41 +0530

    Fixed bugs and deployed this site successfully

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,20 +11,28 @@ const Navbar = () => {
   const navRef = useRef();
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY >= 80) {
-        navRef.current.classList.add("nav-dark");
-      } else {
-        navRef.current.classList.remove("nav-dark");
+    const handleScroll = () => {
+      if (navRef.current) {
+        if (window.scrollY >= 80) {
+          navRef.current.classList.add("nav-dark");
+        } else {
+          navRef.current.classList.remove("nav-dark");
+        }
       }
-    });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
     <>
       <div ref={navRef} className="navbar">
         <div className="navbar-left">
-          <img src={logo} alt="" />
+          <img src={logo} alt="Logo" />
           <ul>
             <li>Home</li>
             <li>Tv Shows</li>
@@ -35,19 +43,19 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-right">
-          <img className="icons" src={search_icon} alt="" />
+          <img className="icons" src={search_icon} alt="Search Icon" />
           <p>Children</p>
-          <img className="icons" src={bell_icon} alt="" />
+          <img className="icons" src={bell_icon} alt="Bell Icon" />
           <div className="navbar-profile">
-            <img className="profile" src={profile_img} alt="" />
-            <img src={caret_icon} alt="" />
+            <img className="profile" src={profile_img} alt="Profile" />
+            <img src={caret_icon} alt="Caret Icon" />
             <div className="dropdown">
               <p
                 onClick={() => {
                   logout();
                 }}
               >
-                Sing Out of Netflix
+                Sign Out of Netflix
               </p>
             </div>
           </div>
